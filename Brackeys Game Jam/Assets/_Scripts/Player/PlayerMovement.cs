c35682256e96d7e55d189dc48f83f0c6bafec5ae
DungNC1 <142099972+DungNC1@users.noreply.github.com>
2025-08-25 21:15:11 +0700

    Dashing

--- Brackeys Game Jam/Assets/_Scripts/Player/PlayerMovement.cs
+++ Brackeys Game Jam/Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,58 +1,98 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed = 8f;
     public float jumpForce = 16f;
 
+    [Header("Dash")]
+    public float dashForce = 20f;
+    public float dashCooldown = 1f;
+
     [Header("Ground Check")]
+    public LayerMask groundLayer;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
 
-    [Header("Coyote Time")]
+    [Header("Jump Buffer & Coyote Time")]
     public float coyoteTime = 0.2f;
-    private float coyoteTimeCounter;
-
-    [Header("Jump Buffer")]
     public float jumpBufferTime = 0.2f;
-    private float jumpBufferCounter;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
+    private bool isDashing = false;
+    private float lastDashTime;
     private float moveInput;
 
-    void Awake()
+    private float lastGroundedTime;
+    private float lastJumpInputTime;
+    private int facingDirection = 1;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if (moveInput != 0)
+        {
+            facingDirection = (int)Mathf.Sign(moveInput);
+            transform.localScale = new Vector3(facingDirection, 1, 1);
+        }
 
-        if (isGrounded)
-            coyoteTimeCounter = coyoteTime;
-        else
-            coyoteTimeCounter -= Time.deltaTime;
+        if (IsGrounded())
+            lastGroundedTime = Time.time;
 
         if (Input.GetButtonDown("Jump"))
-            jumpBufferCounter = jumpBufferTime;
-        else
-            jumpBufferCounter -= Time.deltaTime;
+            lastJumpInputTime = Time.time;
 
-        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time > lastDashTime + dashCooldown)
+            StartCoroutine(Dash());
+
+        if (!isDashing)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            jumpBufferCounter = 0;
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+            if (Time.time - lastJumpInputTime <= jumpBufferTime &&
+                Time.time - lastGroundedTime <= coyoteTime)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                lastJumpInputTime = -1;
+            }
         }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        if (!isDashing)
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        lastDashTime = Time.time;
+
+        float dashDirection = Mathf.Sign(transform.localScale.x);
+        rb.linearVelocity = new Vector2(dashDirection * dashForce, 0f);
+
+        yield return new WaitForSeconds(0.2f);
+        isDashing = false;
     }
 }
